15fc957af5607f954e0b3bb6b429ae9e4309e520
novara754 <novara754@gmail.com>
2024-10-07 00:24:14 +0200

    make spinlock disable interrupts on acquire (at least for now)

--- kernel/src/Spinlock.zig
+++ kernel/src/Spinlock.zig
@@ -3,12 +3,22 @@
 const x86 = @import("x86.zig");
 
 locked: bool = false,
+disabledInterrupts: bool = false,
 
 const Self = @This();
 
 /// Acquire the lock. If the lock is currently held this will enter a spin loop
 /// until the lock can be acquired.
 pub fn acquire(self: *Self) void {
+    // TODO: would be nice to remove this in the future and handle
+    // deadlocks in a way that wouldnt require turning off interrupts
+    // completely
+    if (x86.interruptsEnabled()) {
+        self.disabledInterrupts = true;
+        x86.cli();
+    } else {
+        self.disabledInterrupts = false;
+    }
     while (@atomicRmw(bool, &self.locked, .Xchg, true, .acquire)) {
         x86.pause();
     }
@@ -17,4 +27,7 @@ pub fn acquire(self: *Self) void {
 /// Release the lock.
 pub fn release(self: *Self) void {
     @atomicStore(bool, &self.locked, false, .release);
+    if (self.disabledInterrupts) {
+        x86.sti();
+    }
 }
